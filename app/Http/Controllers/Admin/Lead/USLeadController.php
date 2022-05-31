322f292784941e09a6a49dfca2674b31d210657e
tomjameson <tjameson1061@icloud.com>
2022-05-31 10:05:25 +0100

    stopgo

--- app/Http/Controllers/Admin/Lead/USLeadController.php
+++ app/Http/Controllers/Admin/Lead/USLeadController.php
@@ -172,9 +172,10 @@ class USLeadController extends Controller
         // Add Partner Log
         $partner_log = $this->prepare_log_data($post);
         Log::debug('PARTNER LOG::', (array)$partner_log);
+
         // Get Generated Lead ID
-        $post['lead_id'] = $partner_log->lead_id;
-//        Log::debug('LEAD PASSED INITIAL VALIDATION 2');
+//        $partner_log_id = $partner_log->lead_id;
+
         Log::debug('POST2::', (array)$post);
         // Status Check Code
         $status_check = $this->CheckStatusStart($post);
