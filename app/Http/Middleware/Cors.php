26773027550e72e71360586db11c312d05532054
tomjameson <tjameson1061@icloud.com>
2022-06-22 17:56:11 +0100

    ip

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,9 +18,12 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin:  *');
+       // header('Access-Control-Allow-Origin:  *');
+        header('Access-Control-Allow-Origin: *');
+        header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+
 //        header('Access-Control-Allow-Origin:  http://localhost:4200');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+
         header('Access-Control-Allow-Methods:  POST, PUT');
 
         return $next($request);
