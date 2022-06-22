2d2f5c9e4115285e2b52582664ae32ddcd0a42c3
tomjameson <tjameson1061@icloud.com>
2022-06-22 19:45:02 +0100

    ip

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
+        return $next($request)->header('Access-Control-Allow-Origin', 'https://loanie.net/')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', true)
             ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
