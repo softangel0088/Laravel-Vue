ae2f0cfce1a539f8219643097e55e08e624b0fcb
tomjameson <tjameson1061@icloud.com>
2022-06-22 18:53:34 +0100

    ip

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,19 +13,15 @@ class Cors
      * Handle an incoming request.
      *
      * @param Request $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-       // header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Origin: *');
-        header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
-
-//        header('Access-Control-Allow-Origin:  http://localhost:4200');
-
-        header('Access-Control-Allow-Methods:  POST, PUT');
-
-        return $next($request);
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');
     }
 }
