ca162fd5f9034d3f309e56e16462095d92ad3b75
tomjameson <tjameson1061@icloud.com>
2022-06-24 14:31:17 +0100

    api

--- app/Buyerapis/paydayus/stopgonetworks.php
+++ app/Buyerapis/paydayus/stopgonetworks.php
@@ -252,7 +252,6 @@ class stopgonetworks
 //        dd($lead);
 
         Log::debug('POST DATA::', (array)$lead);
-//        die();
 
         $this->response['post_data'] = $lead;
         $this->response['timeout'] = $client_detail->timeout;
