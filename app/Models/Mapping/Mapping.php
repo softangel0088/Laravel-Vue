ba3a14ed1c887fab8dc9ee105c1b9fb7c428c0ad
Tom <tom@Toms-MacBook-Pro.local>
2022-07-25 10:22:08 +0100

    latest

--- app/Models/Mapping/Mapping.php
+++ app/Models/Mapping/Mapping.php
@@ -106,12 +106,12 @@ class Mapping extends Model
 //            $buyers->where('buyer_setups.timeout', '<=', $search['timeout']);
 //        }
 
-        if (!empty($search['min_price'] && $search['min_price'] !== '0.00')) {
-            $buyers->where('buyer_setups.tier_price', '>=', $search['min_price']);
-        }
-        if (!empty($search['max_price'] && $search['max_price']  !== '0.00')) {
-            $buyers->where('buyer_setups.tier_price', '<=', $search['max_price']);
-        }
+//        if (!empty($search['min_price'] && $search['min_price'] !== '0.00')) {
+//            $buyers->where('buyer_setups.tier_price', '>=', $search['min_price']);
+//        }
+//        if (!empty($search['max_price'] && $search['max_price']  !== '0.00')) {
+//            $buyers->where('buyer_setups.tier_price', '<=', $search['max_price']);
+//        }
 
         if (!empty($search['mode'])) {
             $buyers = $buyers->where('buyer_setups.mode', $search['mode']);
